bed23806a5abfe6873a27529e16d20bf61187748
Renzolucas <mineorigiteste@Gmail.com>
2026-07-22 23:45:15 -0300

    feat: adicionando logica da rota delete

--- src/main/java/com/lucas/agendamento_horario/infrastructure/repository/AgendaentoRepository.java
+++ src/main/java/com/lucas/agendamento_horario/infrastructure/repository/AgendaentoRepository.java
@@ -7,6 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lucas.agendamento_horario.infrastructure.repository.entity.AgendamentoEntity;
 import com.lucas.agendamento_horario.infrastructure.repository.entity.ServicosSalao;
 
+import jakarta.transaction.Transactional;
+
 public interface AgendaentoRepository extends JpaRepository<AgendamentoEntity, Long>{
     AgendamentoEntity findByServicoAndDataHoraAgendamentoBetween(ServicosSalao servico, LocalDateTime dataInicio, LocalDateTime dataFim);
+
+    @Transactional
+    void deleteByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
+
+    AgendamentoEntity findByDataHoraAgendamentoBetween(LocalDateTime horaInicial, LocalDateTime horaFinal);
+
+    AgendamentoEntity findByAndClienteAndDataHoraAgendamento(String cliente, LocalDateTime dataHoraAgendamento);
 }
--- src/main/java/com/lucas/agendamento_horario/services/AgendamentoService.java
+++ src/main/java/com/lucas/agendamento_horario/services/AgendamentoService.java
@@ -1,9 +1,13 @@
 package com.lucas.agendamento_horario.services;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.lucas.agendamento_horario.infrastructure.repository.AgendaentoRepository;
 import com.lucas.agendamento_horario.infrastructure.repository.entity.AgendamentoEntity;
+import com.lucas.agendamento_horario.infrastructure.repository.entity.ServicosSalao;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +16,25 @@ import lombok.RequiredArgsConstructor;
 public class AgendamentoService {
     private final AgendaentoRepository agendamentoRepository;
 
-    public AgendamentoEntity salvarAgendamento(AgendamentoEntity agendamento){
+    //SALVAR AGENDAMENTO
+    public AgendamentoEntity salvarAgendamento(AgendamentoEntity agendamento, ServicosSalao servico){
+        LocalDateTime horaAgendamento = agendamento.getDataHoraAgendamento();
+        LocalDateTime horaFim = horaAgendamento.plusHours(1);
+
+        agendamento.setServicos(servico);
         
+        AgendamentoEntity agendados = agendamentoRepository.findByServicoAndDataHoraAgendamentoBetween(servico, horaAgendamento, horaFim);
+
+        if(agendados != null){
+            throw new RuntimeException("HORARIO INDISPONIVEL");
+        }
+
+        return agendamentoRepository.save(agendamento);
+        
+    }
+    //DELETAR AGENDAMENTO COM BASE NO NOME E HORA
+    public void deletarAgendamento(LocalDateTime dataHoraAgendamento, String cliente){
+        agendamentoRepository.deleteByDataHoraAgendamentoAndCliente(dataHoraAgendamento, cliente);
+
     }
 }
