9b0481677cbefa56164134f8982edd5e54d3527b
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-07-27 18:24:21 -0300

    feat: adicionando anotações relacionadas ao repository

--- src/main/java/com/lucas/agendamento_horario/infrastructure/repository/AgendaentoRepository.java
+++ src/main/java/com/lucas/agendamento_horario/infrastructure/repository/AgendaentoRepository.java
@@ -10,12 +10,18 @@ import com.lucas.agendamento_horario.infrastructure.repository.entity.ServicosSa
 import jakarta.transaction.Transactional;
 
 public interface AgendaentoRepository extends JpaRepository<AgendamentoEntity, Long>{
+    //Verificador de existência de agendamento com base no serviço e nas data/hora incial e final. Possivel através do Between do JPA.
+    //Retorna true se existir agendamento, false caso contrário.
     boolean existsByServicosAndDataHoraAgendamentoBetween(ServicosSalao servico, LocalDateTime dataInicio, LocalDateTime dataFim);
 
+    //O @Transactional é necessário para que o spring boot consiga deletar o agendamento com base na data/hora e no cliente.
     @Transactional
+    //ROTA DELETE: deleta o agendamento com base na data/hora e no cliente.
     void deleteByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
 
+    //ROTA GET: encontra o agendamento com base na data/hora
     AgendamentoEntity findByDataHoraAgendamentoBetween(LocalDateTime horaInicial, LocalDateTime horaFinal);
 
+    //ROTA GET: encontra o agendamento com base no cliente e na data/hora
     AgendamentoEntity findByAndClienteAndDataHoraAgendamento(String cliente, LocalDateTime dataHoraAgendamento);
 }
